4b3ff42ba1ba77e4cc9e1947dabbf5f30a4e5ac8
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-08-19 02:26:15 +0100

    Enum

--- Struct/Program.cs
+++ Struct/Program.cs
@@ -4,22 +4,30 @@ class Program
         struct Product
         {
                 static void Main(string[] args){
-                        Product mouse = new Product(1, "mouse gamer", 34.65);
+                        Product mouse = new Product(1, "mouse gamer", 34.65, EProductType.Product);
                         Console.WriteLine(mouse.Id);
                         Console.WriteLine(mouse.Name);
                         Console.WriteLine(mouse.Price);
+                        Console.WriteLine(mouse.Type);
                 }
-                public Product(int id, string name, double price){
+                public Product(int id, string name, double price, EProductType type){
                         Id = id;
                         Name = name;
                         Price = price;
+                        Type = type;
                 }
                 public int Id;
                 public string Name;
                 public double Price;
+                public EProductType Type;
 
                 public double PriceInDolar(double dolar){
                         return Price * dolar;
                 }
+        }
+
+        enum EProductType{
+                Product = 1,
+                Service = 2
         }
 }
